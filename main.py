6ce67bc15dd0f25ead4c56ec92a4076ab6c7523f
huyilin <huyilin@apusapps.com>
2023-01-13 21:18:05 +0800

    :lipstick:  添加基本功能

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+import ok
+from kivy.clock import Clock
 
-
-class ButtonApp(App):
+class MyApp(App):
     def build(self):
-        return Button(text="Button")
+        self.layout = BoxLayout(orientation='vertical')
+        self.result_label = Label(text="")
+        self.layout.add_widget(self.result_label)
+        self.button = Button(text="Get Time")
+        self.button.bind(on_press=self.get_time)
+        self.layout.add_widget(self.button)
+        self.loading_label = Label(text="Loading...")
+        self.loading_label.opacity = 0
+        self.layout.add_widget(self.loading_label)
+        return self.layout
 
+    def get_time(self, instance):
+        self.loading_label.opacity = 1
+        Clock.schedule_once(self.get_time_async, 0)
 
-ButtonApp().run()
+    def get_time_async(self, dt):
+        current_time = ok.get_time()
+        self.result_label.text = "Current Time: " + current_time
+        self.loading_label.opacity = 0
+
+MyApp().run()
+
---
+++ ok.py
@@ -0,0 +1,4 @@
+import time
+def get_time():
+    time.sleep(3)
+    return str(time.ctime())
